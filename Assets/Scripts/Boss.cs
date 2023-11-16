81d71c0db6b43bdfc12f0bb3f7eb9d5fe670b938
Zook-coder <Zooklenon11@gmail.com>
2023-11-16 02:00:03 +0100

    add array of vector2 for boss path

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -12,6 +12,12 @@ public class Boss : MonoBehaviour
     private int healthBarNumber = 2;
     private readonly float cooldown = 5;
 
+    private float teleportationTimer = 1;
+    private Vector2 Original_coords;
+    private List<Vector2> list_of_coords;
+
+    private int actualBossPointNumber;
+
     [SerializeField]
     private HealthBar healthBar;
 
@@ -27,21 +33,24 @@ public class Boss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // get original position of the boss
+        Original_coords = gameObject.transform.position;
         healthBar.setMaxHealth(health);
         currentHealthbar = healthBarNumber;
         textInput.SetText(currentHealthbar.ToString());
-        gameObject.transform.position = new Vector2(0, 12);
+        gameObject.transform.position = new Vector2(0, 6);
         Debug.Log("Boss has spawned");
+        Debug.Log("Boss coords are " + Original_coords);
+        actualBossPointNumber = 0;
+        list_of_coords = new List<Vector2> { Original_coords, new Vector2(0, -2), Original_coords, new Vector2(4,2), new(6,-2)};
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.y > 6)
-        {
-            rb.MovePosition(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.1f));
-        }
+            
         Shoot();
+        teleportationPatern2();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -79,5 +88,99 @@ public class Boss : MonoBehaviour
         Instantiate(enemyBullet, gameObject.transform.position, Quaternion.identity);
 
 
+    }
+    void teleportationPatern1()
+        {       
+
+        // where am i ?
+        Vector2 currentPos = gameObject.transform.position;
+
+        if ((currentPos == Original_coords) || (currentPos == Original_coords + new Vector2(2, -2)))
+        {   
+            finalPosition = Original_coords + new Vector2(2, 2);
+        }else if (currentPos == Original_coords + new Vector2(2, 2))
+        {
+            finalPosition = Original_coords + new Vector2(-2, 2);
+        }else if (currentPos == Original_coords + new Vector2(-2, 2))
+        {
+            finalPosition = Original_coords + new Vector2(-2, -2);
+        }else if (currentPos == Original_coords + new Vector2(-2, -2))
+        {
+            finalPosition = Original_coords + new Vector2(2, -2);
+        }
+        teleportToCoords(finalPosition);
+    }
+    void teleportationPatern2()
+    {
+        
+        teleportationTimer -= Time.deltaTime;
+        if (teleportationTimer < 0)
+        {
+            var aimed_position = list_of_coords[actualBossPointNumber];
+            Vector2 currentPos = gameObject.transform.position;
+            // if not at the right position
+            Debug.Log("Aimedpoint is " + actualBossPointNumber);
+            Debug.Log("Boss is at " + currentPos);
+            Debug.Log("Aimed position is " + aimed_position);
+            Debug.Log("Distance in x is " + Mathf.Abs(currentPos.x - aimed_position.x));
+
+
+            //if too far from the aimed position x
+            if (Mathf.Abs(currentPos.x - aimed_position.x) > 0.05f)
+            {
+                // if need to go left or right
+                if ((aimed_position.x - currentPos.x) < 0.05f)
+                {   
+                    Debug.Log(aimed_position.x);
+                    teleportToCoords(currentPos + new Vector2(-0.1f, 0));
+                }
+                else
+                {
+                    teleportToCoords(currentPos + new Vector2(0.1f, 0));
+                }
+            }
+            // if too far from the aimed position y
+            else if (Mathf.Abs(currentPos.y - aimed_position.y) > 0.05f)
+            {
+                // if need to go up or down
+                if ((aimed_position.y - currentPos.y) < 0.05f)
+                {
+                    teleportToCoords(currentPos + new Vector2(0, -0.1f));
+                }
+                else
+                {   
+                    teleportToCoords(currentPos + new Vector2(0, +0.1f));
+                }
+            } 
+            // he is at the right position
+            else
+            { // go to the next point
+                Debug.Log("Boss is at the right position");
+                actualBossPointNumber++;
+            }
+            if (actualBossPointNumber > (list_of_coords.Count - 1))
+            {
+                Debug.Log("End of the list");
+                // go back to the first point
+                actualBossPointNumber = 0;
+            }
+
+        }
+    }
+
+
+
+
+    // teleporter to (x,y) coords
+    // wait for 5 seconds => timer1
+    // teleport back to original position
+    // wait for 5 seconds => timer2
+    // repeat
+
+    void teleportToCoords(Vector2 coords)
+    {
+            // teleportation !!!
+            gameObject.transform.position = coords;
+            teleportationTimer = 0.01f;
     }
 }
